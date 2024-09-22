d0ed435b8cb57f41c83fbe7f072552378c0655b8
SkY <you@example.com>
2024-09-22 18:57:41 +0530

    database is up

--- index.js
+++ index.js
@@ -1,12 +1,20 @@
 const express = require("express");
-const app = express();
+const mongoose = require("mongoose");
 
 const { userRouter } = require("./routes/user");
 const { courseRouter } = require("./routes/course");
 const { adminRouter } = require("./routes/admin");
+const app = express();
 
 app.use("/user", userRouter);
-app.use("/course", courseRouter);
 app.use("/admin", adminRouter);
+app.use("/course", courseRouter);
 
-app.listen(3000);
+
+async function main() {
+    await mongoose.connect("mongodb+srv://admin:admin@cluster0.3qgszuq.mongodb.net/coursify-app")
+    app.listen(3000);
+    console.log("listening on port 3000")
+}
+
+main()
